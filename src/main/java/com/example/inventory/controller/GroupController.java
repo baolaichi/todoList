10132eb6fe0ae09bca67434ce23e7b363325ebec
bao_lai 赖志宝 <bao_lai@accton.com.vn>
2025-11-22 16:57:51 +0700

    update

--- src/main/java/com/example/inventory/controller/GroupController.java
+++ src/main/java/com/example/inventory/controller/GroupController.java
@@ -1,9 +1,26 @@
 package com.example.inventory.controller;
 
+import com.example.inventory.model.dto.CreateGroupDTO;
+import com.example.inventory.service.GroupService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/user/group")
 public class GroupController {
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createGroup(@RequestBody CreateGroupDTO groupDTO){
+        String username = SecurityContextHolder.getContext(). getAuthentication().getName();
+        return  ResponseEntity.ok(groupService.createGroup(groupDTO, username));
+    }
 }
